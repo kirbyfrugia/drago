f0b1ff0f720fc6aaff03631245a8e6424212f773
Kirby Frugia <kirby.frugia@gmail.com>
2024-12-31 11:25:15 -0800

    started move from vice to denise

---
+++ ass-drago.sh
@@ -0,0 +1,5 @@
+
+#!/bin/sh
+
+java -jar KickAss.jar drago.asm -log bin/drago_BuildLog.txt -binfile -o bin/drago.bin -vicesymbols -showmem -odir bin
+
---
+++ ass-maped.sh
@@ -0,0 +1,5 @@
+
+#!/bin/sh
+
+java -jar KickAss.jar maped.asm -log bin/maped_BuildLog.txt -binfile -o bin/maped.bin -vicesymbols -showmem -odir bin
+
